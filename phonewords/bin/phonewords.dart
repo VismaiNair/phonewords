787c540a0478f501b6470a7f525b263171622422
Vismai Nair <vismai.nair@gmail.com>
2025-04-18 13:02:21 -0700

    Convert into empty Pub library

---
+++ phonewords/bin/phonewords.dart
@@ -0,0 +1,5 @@
+import 'package:phonewords/phonewords.dart' as phonewords;
+
+void main(List<String> arguments) {
+  print('Hello world: ${phonewords.calculate()}!');
+}
---
+++ phonewords/test/phonewords_test.dart
@@ -0,0 +1,8 @@
+import 'package:phonewords/phonewords.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
